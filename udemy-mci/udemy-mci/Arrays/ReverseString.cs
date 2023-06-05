38fcad3dc5c29e15db97e1127d0abd3a14608c00
Martin Manomat <manomatmartin@gmail.com>
2023-06-05 15:26:36 -0400

    add array folder; add more to gitignore

---
+++ udemy-mci/udemy-mci/Arrays/ReverseString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace udemy_mci.Arrays
+{
+    public class ReverseString
+    {
+        public static void Test()
+        {
+            Console.WriteLine("reverse");
+        }
+    }
+}
